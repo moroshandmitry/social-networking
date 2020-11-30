0f8555f0817d6d0386f670dc5f264f799baa3329
moroshandmitry <moroshandmitry@gmail.com>
2020-11-30 15:03:32 +0200

    add sass, create components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+        <header className={'header'}>
+            <img className={'logo'} src={'https://cdn.worldvectorlogo.com/logos/google-messages.svg'} alt={'Logo'} />
+        </header>
+  );
+}
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Profile = () => {
+  return (
+        <div className={'content'}>
+            <div>
+                <img src={'https://wardleonard.com/wp-content/uploads/2016/04/back_globalsolutions-1200x300.jpg'} alt={'Main content'} />
+            </div>
+            <div>
+                Ava + descriptions
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
